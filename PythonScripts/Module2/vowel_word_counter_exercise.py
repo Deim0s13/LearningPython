858b0b2691b1491231cd076d2aa34760ed69ec00
Deim0s13 <leathen@gmx.de>
2024-01-13 12:32:52 +1300

    Completed Module 2

---
+++ PythonScripts/Module2/vowel_word_counter_exercise.py
@@ -0,0 +1,45 @@
+def vowel_counter(string):
+    """Counts the number of vowels in given string"""
+
+    vowel_count = count_instance_of_str(string, 'aeiou')
+
+    return vowel_count
+
+def word_counter(sentence):
+    """Counts the number of words in a given sentence."""
+
+    sentence = sentence.strip() #removes whitespace from beginning and end of sentence
+
+    num_spaces = count_instance_of_str(sentence, ' ')
+
+    word_count = num_spaces + 1
+
+    return word_count
+
+def count_instance_of_str(string1, string2):
+    "Counts characters in string1 that are also in string2."""
+
+    count = 0
+
+    #for each char in string1, check if it's in string2
+    for char in string1:
+        if char in string2:
+            count += 1
+
+    return count
+
+def main():
+
+    while 1 == 1:
+
+        input_string = input("Enter a string: ")
+
+        if input_string == '-1':
+            break
+
+        print(vowel_counter(input_string), "vowel in", input_string)
+        #print(word_counter(input_string), "words in", input_string)
+
+#execute main function
+if __name__ == '__main__':
+    main()
