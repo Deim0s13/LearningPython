64d1c202894164409935112234610119feffbeb0
Deim0s13 <leathen@gmx.de>
2024-01-15 11:18:49 +1300

    Finished More about lists

---
+++ PythonScripts/Module3/updating_lists.py
@@ -0,0 +1,8 @@
+odd_numbers = [2, 4, 6, 8]
+#  update single element at index 0
+odd_numbers[0] = 1
+print(odd_numbers)
+
+# update multiple elements with slice
+odd_numbers[1:4] = [3, 5, 7]
+print(odd_numbers)
