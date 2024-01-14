76efa336d5723a8a3bf7cdfce86c66b4fabc5144
Deim0s13 <leathen@gmx.de>
2024-01-15 11:29:39 +1300

    Finished Strings

---
+++ PythonScripts/Module3/name_slice.py
@@ -0,0 +1,8 @@
+name = 'Patrick Leathen'
+
+# get index of single space between first and last name
+first_space = name.index(' ')
+
+# get substring of name using slice
+# first name only
+print(name[0:first_space])
---
+++ PythonScripts/Module3/slicing_strings.py
@@ -0,0 +1,4 @@
+s = 'Hello World'
+
+# get substring using slice
+print(s[:5])  # characters 1 - 5
